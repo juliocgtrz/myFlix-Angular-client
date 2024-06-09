f732f97cf0e4a28bc71fc7aef6f0a41085999891
Julio G <juliocgtrz@gmail.com>
2024-06-08 22:24:41 -0600

    Update user-profile.component.ts
    imported components/pages

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -4,171 +4,179 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 
+/** import components */
+import { DirectorInfoComponent } from '../director-info/director-info.component';
+import { GenreInfoComponent } from '../genre-info/genre-info.component';
+import { MovieSynopsisComponent } from '../movie-synopsis/movie-synopsis.component';
+
+/** component for user profile page */
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
 export class UserProfileComponent implements OnInit {
-  @Input() userData: any = { username: '', password: '', email: '', birthday: '' };
+  @Input() userData: any = { Username: '', Password: '', Email: '', Birthday: '', FavoriteMovies: [] };
 
-  formUserData: any = {
-    username: '',
-    password: '',
-    email: '',
-    birthday: '',
-    favoriteMovie: []
-  };
 /** user object */
   user: any = {};
 /** list all movies */
   movies: any[] = [];
 /** list of favorite movies */
-  favoriteMovies: any[] = [];
-/** list of favorite movie IDs */
-  favoriteMoviesIDs: any[] = [];
+  FavoriteMovies: any[] = [];
 
+  /**
+   * constructs UserProfileComponent
+   * @param fetchApiData - the service for fetching API data
+   * @param dialog - the dialog service to display dialogs
+   * @param snackBar - the snack bar service for displaying notifications
+   * @param router - the service for navigation
+   */
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialog: MatDialog,
     public snackBar: MatSnackBar,
-    public router: Router
+    public router: Router,
   ) { }
 
+  /** lifecycle hook called after component initialization */
   ngOnInit(): void {
     this.getProfile();
-    this.getMovies();
     this.getFavoriteMovies();
   }
 
+  /** fetch user profile data */
   public getProfile(): void {
-    this.fetchApiData.getUser().subscribe((response: any) => {
-      console.log('response:', response.favoritemovie);
-      this.user = response;
-      this.userData.userName = this.user.userName;
-      this.userData.email = this.user.email;
-      if (this.user.birthday) {
-        let Birthday = new Date(this.user.birthday);
-        if (!isNaN(Birthday.getTime())) {
-          this.userData.birthday = Birthday.toISOString().split('T')[0];
-        }
-      }
-      this.formUserData = { ...this.userData };
-      this.fetchApiData.getAllMovies().subscribe((movies: any[]) => {
-        this.favoritemovie = movies.filter((movie: any) => this.favoriteMoviesIDs.includes(movie._id));
-      });
+    this.user = this.fetchApiData.getUser();
+    this.userData.Username = this.user.Username;
+    this.userData.Email = this.user.Email;
+    this.userData.Birthday = this.user.Birthday;
+    this.fetchApiData.getAllMovies().subscribe((response) => {
+      this.FavoriteMovies = response.filter((movie: any) => this.user.FavoriteMovies.includes(movie._id));
     });
   }
 
+  /** fetch all movies */
   getMovies(): void {
-    this.fetchApiData.getAllMovies().subscribe((response: any) => {
-      if (Array.isArray(response)) {
-        this.movies = response;
-      }
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
       return this.movies;
     });
   }
 
-  isFav(movie: any): boolean {
-    return this.favoriteMoviesIDs.includes(movie._id);
+  /** fetch user's favorite movies */
+  getFavoriteMovies(): void {
+    this.user = this.fetchApiData.getUser();
+    this.userData.FavoriteMovies = this.user.FavoriteMovies;
+    this.FavoriteMovies = this.user.FavoriteMovies;
+    console.log('Favorite Movies in getFavoriteMovie', this.FavoriteMovies);
   }
 
-  toggleFav(movie: any): void {
-    const isFavorite = this.isFav(movie);
-    isFavorite
-      ? this.deleteFavMovies(movie)
-      : this.addFavMovies(movie);
-  }
-
-  addFavMovies(movie: any): void {
-    const user = JSON.parse(localStorage.getItem('user') || '{}');
-    if (user) {
-      this.fetchApiData.addFavouriteMovies(user.userName, movie._id).subscribe((response) => {
-        localStorage.setItem('user', JSON.stringify(response));
-        this.getFavMovies(); // Refresh favorite movies after adding a new one
-        this.snackBar.open(`${movie.movieName} has been added to your favorites`, 'OK', {
-          duration: 1000,
-        });
-      });
+  /**
+   * checks if a movie is in the user's favorites list
+   * @param movie - the movie to check
+   * @returns true if the movie is a favorite, otherwise it is false
+   */
+  isFavorite(movie: any): boolean {
+    const MovieID = movie._id;
+    if (this.FavoriteMovies.some((movie) => movie === MovieID)) {
+      return true;
+    } else {
+      return false;
     }
   }
 
-  deleteFavMovies(movie: any): void {
-    let user = localStorage.getItem('user');
-    if (user) {
-      let parsedUser = JSON.parse(user);
-      this.userData.UserId = parsedUser._id;
-      this.fetchApiData.deleteFavoriteMovie(parsedUser.userName, movie._id).subscribe((response) => {
-        localStorage.setItem('user', JSON.stringify(response));
-        // Remove the movie ID from the favoritemovie array
-        this.favoriteMoviesIDs = this.favoriteMoviesIDs.filter((id) => id !== movie._id);
-        // Fetch the user's favorite movies again to update the movie list
-        this.getFavMovies();
-        // Show a snack bar message
-        this.snackBar.open(`${movie.movieName} has been removed from your favorites`, 'OK', {
-          duration: 1000,
-        });
-      });
-    }
-  }
-
-  updateUser(): void {
-    this.fetchApiData.updateUser(this.formUserData).subscribe((response) => {
-      console.log('User update success:', response);
-      localStorage.setItem('user', JSON.stringify(response));
-      this.snackBar.open('User updated successfully!', 'OK', {
-        duration: 2000,
-      });
-      this.getProfile();
-    }, (error) => {
-      console.log('Error updating user:', error);
-      this.snackBar.open('Failed to update user', 'OK', {
-        duration: 2000,
+  /**
+   * deletes a movie from the user's favorites list
+   * @param movie - the movie to remove
+   */
+  deleteFavoriteMovies(movie: any): void {
+    this.user = this.fetchApiData.getUser();
+    this.userData.Username = this.user.Username;
+    this.fetchApiData.deleteFavoriteMovies(movie).subscribe((result) => {
+      localStorage.setItem('user', JSON.stringify(result));
+      this.getFavoriteMovies();
+      this. getProfile();
+      this.snackBar.open('Movie deleted from your favorites list!', 'OK', {
+        duration: 3000,
       });
     });
   }
 
-  async deleteUser(): Promise<void> {
-    console.log('deleteUser function called:', this.userData.email)
-    if (confirm('Do you want to delete your account?')) {
-      this.fetchApiData.deleteUser().subscribe(() => {
-        this.snackBar.open('Account deleted successfully!', 'OK', {
-          duration: 3000,
-        });
-        localStorage.clear();
-        this.router.navigate(['welcome']);
+  /** updates user data  */
+  updateUser(): void {
+    this.fetchApiData.updateUser(this.userData).subscribe((result) => {
+      console.log('User update success');
+      localStorage.setItem('user', JSON.stringify(result));
+      this.snackBar.open('User update successful', 'OK', {
+        duration: 2000
       });
-    }
+    }, (error) => {
+      console.error('Error updating user:', error);
+      this.snackBar.open('Failed to update user', 'OK', {
+        duration: 2000
+      });
+    });
   }
 
-  openGenreDialog(genre: string, description: string): void {
+  /** delete the user's account */
+  deleteUser(): void {
+    this.router.navigate(['welcome']).then(() => {
+      localStorage.clear();
+      this.snackBar.open('User successfully deleted.', 'OK', {
+        duration: 2000
+      });
+    })
+    this.fetchApiData.deleteUser().subscribe((result) => {
+      console.log(result);
+    });
+  }
+
+  /**
+   * opens a dialog to view genre information for that movie
+   * @param genre  - the genre
+   * @param description - description of the genre
+   */
+  openGenreDialog(name: string, description: string): void {
     this.dialog.open(GenreInfoComponent, {
       data: {
-        genre: genre,
-        description: description
+        Name: name,
+        Description: description,
       },
-      width: '500px',
+      width: '450px',
     });
   }
 
-  openDirectorDialog(director: string, bio: string, birthdate: string): void {
+  /**
+   * opens a dialog to view the director info
+   * @param name - the director's name
+   * @param bio - the director's bio
+   * @param birth - the director's birth year
+   * @param death - year of director's death
+   */
+  openDirectorDialog(name: string, bio: string, birth: string, death: string): void {
     this.dialog.open(DirectorInfoComponent, {
       data: {
-        director: director,
-        bio: bio,
-        birthdate: birthdate,
+        Name: name,
+        Bio: bio,
+        Birth: birth,
+        Death: death
       },
-      width: '500px',
+      width: '450px',
     });
   }
 
-  openSynopsisDialog(movieName: string, description: string): void {
+  /**
+   * opens a dialog to view a synopsis of the movie
+   * @param description - the synopsis of the movie
+   */
+  openSynopsisDialog(description: string): void {
     this.dialog.open(MovieSynopsisComponent, {
       data: {
-        movieName: movieName,
-        description: description
+        Description: description,
       },
-      width: '500px',
+      width: '450px',
     });
   }
 
